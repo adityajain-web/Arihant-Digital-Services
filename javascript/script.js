5fc02604da0696222995816e5a3ee4e505d1ac56
adityajain-web <60445026+adityajain-web@users.noreply.github.com>
2020-12-08 12:45:49 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+    // <!-- ready for business section -->
+    // <section>
+    //     <div class="container">
+    //         <div class="row">
+    //             <div class="col-md-6 col-10 offset-md-0 offset-1 order-md-1 order-2">
+    //                 <div class="d-flex align-items-center py-3 px-3">
+    //                     <div>
+    //                         <h1>Ready for your business to stand out?
+    //                         </h1>
+    //                         <p>Arihant organizes your web presence and marketing efforts in one place, freeing you up to
+    //                             focus on your business. Build a
+    //                             website or webshop for free and add marketing as you please.
+    //                         </p>
+    //                         <button class="btn btn-primary rounded-pill">Get Started</button>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //             <div class="col-md-6 col-10 offset-md-0 offset-1 order-md-2 order-1">
+    //                 <img src="https://www.datocms-assets.com/15524/1588080416-lady-happy-numbers-2x.png?auto=format&dpr=1&w=1078"
+    //                     alt="" class="img-fluid">
+    //             </div>
+    //         </div>
+    //     </div>
+    // </section>
+    // <!-- ready for business section -->
